9e8fe4c1c37a18bff8ca43e70fd3902674e9b232
Morning <morning@admins-iMac-3.local>
2023-03-10 12:47:35 +0800

    val swutch

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,38 @@
-export default function Gallery() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from "./Table.jsx";
+
+export default function Gallery(props) {
+  const { val } = props;
+  const [gallery, setGallery] = useState();
+  const [show, setShow] = useState();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:2050/${val == 3 ? "category" : "brand"}`)
+      .then((res) => setGallery(res.data));
+    setShow(null);
+  }, [val]);
+  if (show) {
+    console.log(show);
+    return <Table val={val} name={show.name} />;
+  }
   return (
     <div className="w-full h-full p-10 gap-10 flex flex-wrap">
-      <div className="flex flex-col w-2/12 border h-80">
-        <div className="w-full bg-blue-500 h-5/6">zrg</div>
-        <div className="">name</div>
-      </div>
+      {gallery &&
+        gallery.map((item, index) => {
+          return (
+            <div
+              className="flex flex-col w-2/12 border h-80"
+              key={index}
+              onClick={() => {
+                setShow({ key: index, name: item.name });
+              }}
+            >
+              <div className="w-full bg-blue-500 h-5/6">zrg</div>
+              <div className="">{item.name}</div>
+            </div>
+          );
+        })}
     </div>
   );
 }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Table(props) {
-  const { val } = props;
+  const { val, name } = props;
+  const [data, setData] = useState([]);
+  const [limit, setLimit] = useState(2);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:2050/products${
+          val == 2
+            ? `?limit=${limit}`
+            : val == 3
+            ? `?category=${name} `
+            : val == 4
+            ? `?brand=${name} `
+            : ""
+        }`
+      )
+      .then((res) => setData([...res.data]));
+  }, [limit, val]);
+
   return (
     <div className="w-full flex flex-col items-center gap-20">
       <table className="w-full">
@@ -17,22 +37,32 @@ export default function Table(props) {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b-2 border-neutral-900">
-            <td className="text-center">url</td>
-            <td className="text-center">iPhone 14</td>
-            <td className="text-center">2000$</td>
-            <td className="text-center">apple</td>
-            <td className="text-center">phone</td>
-            <td className="text-center">10</td>
-            <td className="text-center">edit/delete</td>
-          </tr>
+          {data &&
+            data.map((product, index) => {
+              return (
+                <tr className="border-b-2 border-neutral-900" key={index}>
+                  <td className="text-center">
+                    <img className="w-20" src={product.image} alt="" />
+                  </td>
+                  <td className="text-center">{product.name}</td>
+                  <td className="text-center">{product.price}$</td>
+                  <td className="text-center">{product.brand}</td>
+                  <td className="text-center">{product.category}</td>
+                  <td className="text-center">{product.stock}</td>
+                  <td className="text-center">edit/delete</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
-      {val == 2 && (
-        <button className="focus:outline-none text-white bg-purple-300 hover:bg-purple-400 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 ">
+      {val == 2 && data.length >= limit ? (
+        <button
+          className="focus:outline-none text-white bg-purple-300 hover:bg-purple-400 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 "
+          onClick={() => setLimit(limit + 2)}
+        >
           load more
         </button>
-      )}
+      ) : null}
     </div>
   );
 }
